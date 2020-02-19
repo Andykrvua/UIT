79435821c8b4e48e8ebf42a276d83c4b3405c0dd
Andrey <andrey.kallko@gmail.com>
2020-02-19 18:53:41 +0300

    auto

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Headroom from "headroom.js";
+import Swiper from "swiper";
 
+// dynamic header
 const myElement = document.querySelector("header");
 
 var headroom = new Headroom(myElement, {
@@ -11,3 +13,24 @@ var headroom = new Headroom(myElement, {
   }
 });
 headroom.init();
+
+// accordion
+let acc = document.getElementsByClassName("myth-item");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+  });
+}
+
+// review slider
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
